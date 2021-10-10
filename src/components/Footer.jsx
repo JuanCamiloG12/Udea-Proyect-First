0478a44fcb42afda0b5bd15fba05c114bf343c78
Edward Palacios <eapd1820@gmail.com>
2021-10-10 02:40:01 -0500

    Se incluyen iconos de redes sociales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 
+
 const Footer = () => {
     return (
         <div className='absolute bottom-0 w-full bg-gray-700 text-white'>
@@ -9,9 +10,9 @@ const Footer = () => {
                     <p className='m-2 font-bold'> &copy; All rigth reserved.</p>  
                 </div>
                 <div className='flex'>
-                    <div className='m-2'> <i class="fab fa-facebook-square"></i> Facebook</div>
-                    <div className='m-2'>Instagram</div>
-                    <div className='m-2'>Twitter</div>
+                    <div className='m-2 hover:text-blue-600'> <ion-icon name="logo-facebook"></ion-icon> Facebook</div>
+                    <div className='m-2 hover:text-yellow-500'> <ion-icon name="logo-instagram"></ion-icon> Instagram</div>
+                    <div className='m-2 hover:text-blue-400'><ion-icon name="logo-twitter"></ion-icon> Twitter</div>
                 </div>
             </div>    
         </div>
