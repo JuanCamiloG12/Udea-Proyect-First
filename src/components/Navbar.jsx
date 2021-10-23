2f9c945647ad33a9542cc1a3da2aa4998a4a6703
Juan Camilo <juanfable@gmail.com>
2021-10-23 16:14:41 -0500

    h

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,6 +40,12 @@ const Navbar = () => {
           {/* Mobile Menu open: "block", Menu closed: "hidden" */}
           <div className="flex-1 md:flex md:items-center md:justify-between">
             <div className="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+            <Link
+                to="/"
+                className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700"
+              >
+                Home{" "}
+              </Link>
               <Link
                 to="/AdminPages/Ventas"
                 className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700"
